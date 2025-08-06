6530543035ee627e67c6eaa78b44cb1da505a66a
fahd-74 <fhakem75@gmail.com>
2025-08-06 13:03:39 +0300

    fix: permission

--- src/_modules/authorization/providers/permissions.provider.ts
+++ src/_modules/authorization/providers/permissions.provider.ts
@@ -35,6 +35,12 @@ export const permissions = [
     default: false,
     methods: ['post', 'get', 'patch', 'delete'],
   },
+  {
+    name: { en: 'Banners', ar: 'البانرات' },
+    prefix: 'banners',
+    default: false,
+    methods: ['post', 'get', 'patch', 'delete'],
+  },
 ];
 
 type Permission = (typeof permissions)[number];
