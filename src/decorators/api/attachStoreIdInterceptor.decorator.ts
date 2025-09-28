8154511460226fd31246bbef84da0b1fee6a4622
Hazem172002 <hazmj05@gmail.com>
2025-09-28 15:18:39 +0300

    feat: add AttachStoreId and AttachUserId interceptors for handling store and user IDs in requests

---
+++ src/decorators/api/attachStoreIdInterceptor.decorator.ts
@@ -0,0 +1,69 @@
+import {
+  applyDecorators,
+  BadRequestException,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { RolesKeys } from 'src/_modules/authorization/providers/roles';
+
+@Injectable()
+export class AttachStoreIdInterceptor implements NestInterceptor {
+  constructor(
+    private readonly ignoreGet: boolean = false,
+    private readonly ignorePost: boolean = false,
+    private readonly ignorePatch: boolean = false,
+  ) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const method = request.method.toUpperCase();
+    const user = request.user as CurrentUser;
+
+    const body = request.body;
+    const filters = request.query;
+
+    if (method === 'POST' && !this.ignorePost) {
+      if (user.Role.roleKey === RolesKeys.ADMIN) {
+        if (!body.storeId) {
+          throw new BadRequestException('storeId is required for admin user');
+        }
+      } else {
+ 
+          body.storeId = user.storeId;
+      }
+    }
+
+    if (method === 'GET' && !this.ignoreGet) {
+      if (user.Role.roleKey !== RolesKeys.ADMIN) {
+  
+          filters.storeId = user.storeId;
+      }
+    }
+
+    if (method === 'PATCH' && !this.ignorePatch) {
+      delete body?.storeId;
+    }
+
+    return next.handle();
+  }
+}
+
+export function AttachStoreId(parameters?: {
+  ignoreGet?: boolean;
+  ignorePost?: boolean;
+  ignorePatch?: boolean;
+}) {
+  return applyDecorators(
+    UseInterceptors(
+      new AttachStoreIdInterceptor(
+        parameters?.ignoreGet,
+        parameters?.ignorePost,
+        parameters?.ignorePatch,
+      ),
+    ),
+  );
+}
